28cb1d337911c1e44618cf4f072a46df435253bf
Johannes Thies <thies@soronsystems.com>
2026-06-05 14:36:34 +0200

    style: add select-none to main application container to prevent text highlighting

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import AppHeader from "./components/layout/AppHeader.vue";
 
 <template>
   <div
-    class="flex flex-col min-h-screen w-full bg-bg-light dark:bg-bg-dark text-text-light dark:text-text-dark transition-colors duration-300"
+    class="flex flex-col min-h-screen w-full bg-bg-light dark:bg-bg-dark text-text-light dark:text-text-dark transition-colors duration-300 select-none"
   >
     <AppHeader />
     <main class="grow flex flex-col w-full relative">
